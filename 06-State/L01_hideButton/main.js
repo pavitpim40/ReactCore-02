7aa64168c14e968e89eb8cf6dcca9167789ab339
Pavit Pimchanagul <pavitpim@gmail.com>
2023-08-25 15:32:58 +0700

    sol state

--- 06-State/L01_hideButton/main.js
+++ 06-State/L01_hideButton/main.js
@@ -1,4 +1,50 @@
+function App() {
+    // State
+    const [isShow, setIsShow] = React.useState(true);
+
+    // Render
+    const handleClick = () => setIsShow(!isShow);
+
+    // #1
+    // return <div>{isShow && <button onClick={handleClick}>Click to hide me</button>}</div>;
+
+    // #2
+    // return (
+    //     <div>
+    //         <button onClick={handleClick}>Click to hide text</button>
+    //         {isShow ? <h3>Text</h3> : ''}
+    //     </div>
+    // );
+
+    // #3
+    // return (
+    //     <div>
+    //         <button onClick={handleClick}>{isShow ? 'Hide' : 'Show'}</button>
+    //         {isShow ? <h3>Text</h3> : ''}
+    //     </div>
+    // );
+
+    // แถม
+
+    if (isShow) {
+        // isLoading == true => render Spinner
+        return (
+            <div>
+                <button onClick={handleClick}>Hide</button>
+                <h1>Text</h1>
+            </div>
+        );
+    }
+
+    return (
+        // isLoading == false ==> render App
+        <div>
+            <button onClick={handleClick}>Show</button>
+        </div>
+    );
+}
+
 // ReactDOM
 const domRoot = document.getElementById('root');
 const root = ReactDOM.createRoot(domRoot);
-root.render(<h1>Hello React</h1>);
+root.render(<App />);
