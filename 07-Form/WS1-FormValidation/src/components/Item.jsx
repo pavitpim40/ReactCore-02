a97616f51dd4585901a23a8a4fb562c1bf514ddb
Pavit Pimchanagul <pavitpim@gmail.com>
2023-08-25 17:34:21 +0700

    update form

---
+++ 07-Form/WS1-FormValidation/src/components/Item.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Item() {
+    return <h1>ITEM</h1>;
+    // <h1>ITEM</h1> === React.CreateElement
+}
+
+export default Item;
--- 07-Form/WS1-FormValidation/src/index.js
+++ 07-Form/WS1-FormValidation/src/index.js
@@ -1,12 +1,55 @@
-import './index.css';
-import React from 'react';
-import { createRoot } from 'react-dom';
+// import <ITEM> from <LIB_NAME>
 
-// React
+// Import : OtherCode
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+// Import : OurCode
+import './index.css';
+import Item from './components/Item.jsx';
+
 function App() {
+    const [email, setEmail] = useState(''); // "cc@mail.com"
+    const [password, setPassword] = useState('');
+
+    // EMAIL
+    function handleChangeEmail(event) {
+        // event.target.value ==  "cc@mail.com"
+        setEmail(event.target.value);
+    }
+
+    // PASSWORD
+    function handlePassword(event) {
+        setPassword(event.target.value);
+    }
+
+    // SUBMIT
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('submit');
+    }
     return (
-        <div>
-            <h1>Hello React!</h1>
+        <div className='app'>
+            <h1>Sign Up</h1>
+            <form className='form__register'>
+                <input
+                    type='email'
+                    placeholder='email'
+                    onChange={handleChangeEmail}
+                    value={email}
+                />
+                <input
+                    type='password'
+                    placeholder='password'
+                    onChange={handlePassword}
+                    value={password}
+                />
+                <input type='password' placeholder='confirm password' />
+                <button onClick={handleSubmit} type='submit'>
+                    Sign up with Email
+                </button>
+            </form>
         </div>
     );
 }
@@ -14,4 +57,5 @@ function App() {
 // ReactDOM
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
+
 root.render(<App />);
